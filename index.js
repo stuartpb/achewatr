cef1d23160ba6ac675cc1754d48bde212d7b6ba3
Stuart P. Bentley <stuart@testtrack4.com>
2013-01-03 05:57:43 -0500

    Try fixing blog dating with a clone

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function fortifyItem(doc){
       //If you tried to just change the published date, that would interfere
       //with the previous and last posts in getTrio,
       //so we don't do that, OK?
-      doc.blogDate = XDate(doc.published).addMinutes(doc.offsetmins);
+      doc.blogDate = doc.published.clone().addMinutes(doc.offsetmins);
 
       doc.date = doc.blogDate.toString('ddd MM.dd.yyyy hh:mm TT');
     }
